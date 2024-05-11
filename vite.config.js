e85c22322122914e5300820ae844146fbc058d36
ZTatman <tatmanzachariahit@gmail.com>
2024-05-11 15:20:29 -0400

    refactor: add hono, @hono/vite-dev-server and necessary configurations to use hono as a backend server with vite frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import devServer from '@hono/vite-dev-server';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    server: {
+        port: 3000, // change to a custom port
+    },
+    plugins: [
+        react(),
+        devServer({
+            entry: 'server.js',
+            exclude: [
+                // We need to override this option since the default setting doesn't fit
+                /.*\.jsx?($|\?)/,
+                /.*\.(s?css|less)($|\?)/,
+                /.*\.(svg|png)($|\?)/,
+                /^\/@.+$/,
+                /^\/favicon\.ico$/,
+                /^\/(public|assets|static)\/.+/,
+                /^\/node_modules\/.*/,
+            ],
+            injectClientScript: false, // This option is buggy, disable it and inject the code manually
+        }),
+    ],
+});
